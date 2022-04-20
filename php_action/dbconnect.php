ca8e367bc4a438ac22b1e7639b07f397582783e9
Ongarj Dev <ongarj2314@gmail.com>
2022-04-20 13:31:26 +0700

    version 1.0 pertest

--- php_action/dbconnect.php
+++ php_action/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = new mysqli('us-cdbr-east-05.cleardb.net','b0f6f00b2326a0','053e77f1','heroku_4bd1ea5013e83e0');
+$con = new mysqli('localhost','root','root','dbshop');
 
 if($con->connect_error){
     die("Server config ERROR (check config in server)" . $con->connect_error);
