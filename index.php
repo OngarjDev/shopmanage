3de3c1f0ef24b8d7240bd26e849f06f16fadb9b6
Ongarj Dev <ongarj2314@gmail.com>
2022-06-11 23:15:20 +0700

    1.1.2

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,18 +10,4 @@
 <body>
 ไปที่หน้า<a href="pages/index.php">เข้าสู่ระบบ</a>
 </body>
-=======
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="refresh" content="url=pages/index.php">
-<title>ยินดีต้อนรับ</title>
-<script language="javascript">
-    window.location.href = "pages/index.php"
-</script>
-</head>
-<body>
-ไปที่หน้า<a href="pages/index.php">เข้าสู่ระบบ</a>
-</body>
->>>>>>> 2856649ca7bf589cd8c5976ab14e7a58e7915552
 </html>
